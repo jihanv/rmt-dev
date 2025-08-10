a7b139efeae850dd7f4da9ab16a16fd10a722900
jihanv <hedjmny@gmail.com>
2025-08-10 12:31:04 +0900

    Prevent event bubbling

--- src/components/BookmarkIcon.tsx
+++ src/components/BookmarkIcon.tsx
@@ -9,9 +9,15 @@ export default function BookmarkIcon({ id }: BookmarkIconProps) {
 
   const context = useContext(BookmarksContext);
   if (!context) throw new Error("BookmarkIcon must be used within BookmarkContextProvider");
+
   const { bookmarkedIds, handleToggleBookmark } = context;
+
   return (
-    <button onClick={() => handleToggleBookmark(id)} className="bookmark-btn">
+    <button onClick={(e) => {
+      handleToggleBookmark(id);
+      e.stopPropagation();
+      e.preventDefault()
+    }} className="bookmark-btn">
       <BookmarkFilledIcon className={bookmarkedIds.includes(id) ? "filled" : ""} />
     </button>
   );

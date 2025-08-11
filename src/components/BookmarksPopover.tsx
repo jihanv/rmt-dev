26bec115a12fde6a048a8b0aaddf559cabb53854
jihanv <hedjmny@gmail.com>
2025-08-11 20:37:21 +0900

    Make custom hook to handle clicking outside

--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -1,17 +1,3 @@
-// import { useBookmarksContextProvider } from "../lib/hooks";
-// import JobList from "./JobList";
-
-// export default function BookmarksPopover() {
-
-//   const { bookmarkedJobItems, isLoading } = useBookmarksContextProvider()
-
-//   console.log(bookmarkedJobItems)
-//   return <div className="bookmarks-popover">
-//     {!isLoading && <JobList jobItems={bookmarkedJobItems} isLoading={isLoading} />}
-//   </div>;
-// }
-
-
 import { forwardRef } from "react";
 import { useBookmarksContextProvider } from "../lib/hooks";
 import JobList from "./JobList";
--- src/lib/hooks.ts
+++ src/lib/hooks.ts
@@ -143,3 +143,22 @@ export function useBookmarksContextProvider() {
 
   return context;
 }
+
+export function useOnClickOutside(
+  refs: React.RefObject<HTMLElement>[],
+  handler: () => void
+) {
+  useEffect(() => {
+    const handleClick = (e: MouseEvent) => {
+      if (
+        e.target instanceof HTMLElement &&
+        refs.every((ref) => !ref.current?.contains(e.target as Node))
+      ) {
+        handler();
+      }
+    };
+    document.addEventListener("click", handleClick);
+
+    return () => document.removeEventListener("click", handleClick);
+  }, [refs, handler]);
+}

e9be0479788f613c8a635d06f46329fdd5997895
jihanv <hedjmny@gmail.com>
2025-08-11 19:54:57 +0900

    Close popover using useRef

--- src/components/BookmarksPopover.tsx
+++ src/components/BookmarksPopover.tsx
@@ -1,12 +1,28 @@
+// import { useBookmarksContextProvider } from "../lib/hooks";
+// import JobList from "./JobList";
+
+// export default function BookmarksPopover() {
+
+//   const { bookmarkedJobItems, isLoading } = useBookmarksContextProvider()
+
+//   console.log(bookmarkedJobItems)
+//   return <div className="bookmarks-popover">
+//     {!isLoading && <JobList jobItems={bookmarkedJobItems} isLoading={isLoading} />}
+//   </div>;
+// }
+
+
+import { forwardRef } from "react";
 import { useBookmarksContextProvider } from "../lib/hooks";
 import JobList from "./JobList";
 
-export default function BookmarksPopover() {
+const BookmarksPopover = forwardRef<HTMLDivElement>(
+  function (_, ref) {
+    const { bookmarkedJobItems, isLoading } = useBookmarksContextProvider()
 
-  const { bookmarkedJobItems, isLoading } = useBookmarksContextProvider()
+    return (<div ref={ref} className="bookmarks-popover">
+      {!isLoading && <JobList jobItems={bookmarkedJobItems} isLoading={isLoading} />}
+    </div>);
+  })
 
-  console.log(bookmarkedJobItems)
-  return <div className="bookmarks-popover">
-    {!isLoading && <JobList jobItems={bookmarkedJobItems} isLoading={isLoading} />}
-  </div>;
-}
+export default BookmarksPopover

d648319f178827ea70330213178b7373dc212fd1
jihanv <hedjmny@gmail.com>
2025-08-12 17:10:58 +0900

    Use useMemo for jobsItemsSorted and jobItemsSliced

--- src/contexts/JobItemsContextProvider.tsx
+++ src/contexts/JobItemsContextProvider.tsx
@@ -1,10 +1,9 @@
-import { createContext, useState } from "react"
+import { createContext, useMemo, useState } from "react";
 import { useSearchQuery, useSearchTextContextProvider } from "../lib/hooks";
 import { JobItem, PageDirection, SortBy } from "../lib/types";
 import { RESULTS_PER_PAGE } from "../lib/constants";
 
-
-export const JobItemsContext = createContext<JobItemsContextValue | null>(null)
+export const JobItemsContext = createContext<JobItemsContextValue | null>(null);
 
 type JobItemsContextProviderProps = {
     children: React.ReactNode;
@@ -21,52 +20,58 @@ export type JobItemsContextValue = {
     handleChangePage: (direction: PageDirection) => void;
     handleSortBy: (newSort: SortBy) => void;
 };
-export default function JobItemsContextProvider({ children }: JobItemsContextProviderProps) {
+export default function JobItemsContextProvider({
+    children,
+}: JobItemsContextProviderProps) {
+    const { debouncedSearchText } = useSearchTextContextProvider();
+    const { jobItems, isLoading } = useSearchQuery(debouncedSearchText);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [sortBy, setSortBy] = useState<SortBy>("relevant");
 
-    const { debouncedSearchText } = useSearchTextContextProvider()
-    const { jobItems, isLoading } = useSearchQuery(debouncedSearchText)
-    const [currentPage, setCurrentPage] = useState(1)
-    const [sortBy, setSortBy] = useState<SortBy>("relevant")
-
-    const jobsItemsSorted = [...(jobItems || [])]?.sort((a, b) => {
+    const jobsItemsSorted = useMemo(() => [...(jobItems || [])]?.sort((a, b) => {
         if (sortBy === "relevant") {
-            return b.relevanceScore - a.relevanceScore
+            return b.relevanceScore - a.relevanceScore;
         } else {
-            return a.daysAgo - b.daysAgo
+            return a.daysAgo - b.daysAgo;
         }
-    })
+    }), [sortBy, jobItems]);
 
-    const totalResults = jobItems?.length || 0
-    const totalNumberOfPages = totalResults / RESULTS_PER_PAGE
-    const jobItemsSliced = jobsItemsSorted?.slice(currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE, currentPage * RESULTS_PER_PAGE) || []
+    const totalResults = jobItems?.length || 0;
+    const totalNumberOfPages = totalResults / RESULTS_PER_PAGE;
+    const jobItemsSliced =
+        useMemo(() => jobsItemsSorted?.slice(
+            currentPage * RESULTS_PER_PAGE - RESULTS_PER_PAGE,
+            currentPage * RESULTS_PER_PAGE
+        ), [currentPage, jobsItemsSorted]);
 
     const handleChangePage = (direction: PageDirection) => {
         if (direction === "next") {
-            setCurrentPage(((prev: number) => prev + 1))
+            setCurrentPage((prev: number) => prev + 1);
         } else if (direction === "previous") {
-            setCurrentPage(((prev: number) => prev - 1))
+            setCurrentPage((prev: number) => prev - 1);
         }
-    }
+    };
 
     const handleSortBy = (newSort: SortBy) => {
         setSortBy(newSort);
         setCurrentPage(1);
-    }
-
-
+    };
 
     return (
-        <JobItemsContext.Provider value={{
-            jobItems,
-            jobItemsSliced,
-            isLoading,
-            totalResults,
-            totalNumberOfPages,
-            currentPage,
-            sortBy,
-            handleChangePage,
-            handleSortBy,
-        }}>{children}
+        <JobItemsContext.Provider
+            value={{
+                jobItems,
+                jobItemsSliced,
+                isLoading,
+                totalResults,
+                totalNumberOfPages,
+                currentPage,
+                sortBy,
+                handleChangePage,
+                handleSortBy,
+            }}
+        >
+            {children}
         </JobItemsContext.Provider>
-    )
+    );
 }
